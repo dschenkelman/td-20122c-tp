3406aa3dcc8e6a9d847ab35dd94ed89f65f919fd
servetto.matias@gmail.com <servetto.matias@gmail.com@8d87b73c-e004-1360-0544-0c0580c6776a>
2012-10-20 22:29:06 +0000

    XmlActionReader OK

---
+++ trunk/code/CourseManagement/CourseManagement.EmailProcessing/Actions/IXmlActionReader.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CourseManagement.EmailProcessing.Actions
+{
+    public interface IXmlActionReader
+    {
+        IEnumerable<string> GetActionNames(string rulesConfigurationFilePath, string ruleName);
+    }
+}
