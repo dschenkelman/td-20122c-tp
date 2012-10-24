844bc0ce7200dae3a7598f54cb3373a0338d5442
sebastianr213@gmail.com <sebastianr213@gmail.com@8d87b73c-e004-1360-0544-0c0580c6776a>
2012-10-24 16:09:53 +0000

    Implemented AddGroupToCourseDatabaseEntry Action

--- code/CourseManagement/CourseManagement.Entities/Group.cs
+++ code/CourseManagement/CourseManagement.Entities/Group.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -6,10 +7,20 @@ namespace CourseManagement.Model
 {
     public class Group
     {
+        public Group(int year, int semester)
+        {
+            this.Year = year;
+            this.Semester = semester;
+        }
+
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         public int CourseId { get; set; }
+
+        public int Year { get; set; }
+
+        public int Semester { get; set; }
 
         [ForeignKey("CourseId")]
         public virtual Course Course { get; set; }
