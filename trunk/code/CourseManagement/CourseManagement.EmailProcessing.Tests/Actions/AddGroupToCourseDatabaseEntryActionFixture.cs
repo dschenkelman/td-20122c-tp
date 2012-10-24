0c1acafb747cc0b524d2e267ebfb91cd4e49c214
sebastianr213@gmail.com <sebastianr213@gmail.com@8d87b73c-e004-1360-0544-0c0580c6776a>
2012-10-24 16:09:53 +0000

    Implemented AddGroupToCourseDatabaseEntry Action

---
+++ trunk/code/CourseManagement/CourseManagement.EmailProcessing.Tests/Actions/AddGroupToCourseDatabaseEntryActionFixture.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using CourseManagement.EmailProcessing.Actions;
+using CourseManagement.Model;
+using CourseManagement.Persistence.Repositories;
+using Microsoft.Moles.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace CourseManagement.EmailProcessing.Tests.Actions
+{
+    [TestClass]
+    public class AddGroupToCourseDatabaseEntryActionFixture
+    {
+        private MockRepository mockRepository;
+        private Mock<ICourseManagementRepositories> courseManagementRepostiories;
+        private Mock<IRepository<Group>> groupRepository;
+        private Mock<IEmail> email;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+            this.courseManagementRepostiories = this.mockRepository.Create<ICourseManagementRepositories>();
+            this.groupRepository = this.mockRepository.Create<IRepository<Group>>();
+
+            this.courseManagementRepostiories.Setup(cmr => cmr.Groups).Returns(this.groupRepository.Object);
+            
+            this.email = this.mockRepository.Create<IEmail>();
+        }
+
+        [TestMethod]
+        public void ShouldAddNewGroupWhenUsingExecute()
+        {
+            // arrange
+            const int correctYear = 2012;
+            const int incorrectYear = 2000;
+
+            const int correctSemester = 2;
+            const int incorrectSemester = 1;
+
+            var trueGroup = new Group(correctYear,correctSemester);
+
+            var falseGroupWrongYearAndSemester = new Group(incorrectYear, incorrectSemester);
+
+            var falseGroupWrongYear = new Group(incorrectYear, correctSemester);
+
+            var falseGroupWrongSemester = new Group(correctYear, incorrectSemester);
+
+            //TODO Habria que hacer lo mismo que con el insert
+            this.groupRepository.Setup(gr => gr.Get(It.IsAny<Expression<Func<Group, bool>>>())).Returns(new List<Group>()).Verifiable();
+
+            //TODO Fijarse porque no funciona.
+            //this.groupRepository.Setup(gr => gr.Insert(It.Is<Expression<Func<Group, bool>>>(f => f.Compile().Invoke(trueGroup)))).Verifiable();)
+            this.groupRepository.Setup(gr => gr.Insert(It.IsAny<Group>())).Verifiable();
+
+            this.groupRepository.Setup(gr => gr.Save()).Verifiable();
+
+            AddGroupToCourseDatabaseEntryAction action = this.CreateAddGroupToCourseDatabaseEntryAction();
+
+            Mock<IEmail> email = mockRepository.Create<IEmail>();
+
+            // act
+
+            action.Execute(email.Object);
+
+            // assert
+
+            this.groupRepository.Verify(gr => gr.Get(It.IsAny<Expression<Func<Group, bool>>>()), Times.Once());
+            //TODO Cambiar el insert con It.IsAny por el de It.Is cuando se resuelva
+            this.groupRepository.Verify(gr => gr.Insert(It.IsAny<Group>()), Times.Once());
+            this.groupRepository.Verify(gr => gr.Save(), Times.Once());
+
+        }
+
+        [ExpectedException(typeof(Exception))]
+        [TestMethod]
+        public void ShouldNotAddAlreadyExistingGroupToCourseDatabaseWhenUsingExecute()
+        {
+            // arrange
+            const int year = 2012;
+            const int semester = 2;
+            var group = new Group(year, semester);
+
+            var groups = new List<Group> {group};
+
+            this.groupRepository.Setup(gr => gr.Get(It.IsAny<Expression<Func<Group,bool>>>())).Returns(groups).Verifiable();
+            this.groupRepository.Setup(gr => gr.Insert(It.IsAny<Group>())).Verifiable();
+            this.groupRepository.Setup(gr => gr.Save()).Verifiable();
+  
+            AddGroupToCourseDatabaseEntryAction action = this.CreateAddGroupToCourseDatabaseEntryAction();
+
+            Mock<IEmail> email = mockRepository.Create<IEmail>();
+            
+            // act
+            action.Execute(email.Object);
+
+            // assert
+            this.groupRepository.Verify(gr => gr.Insert(It.IsAny<Group>()), Times.Never());
+            this.groupRepository.Verify(gr => gr.Save(), Times.Never());
+
+        }
+
+        private AddGroupToCourseDatabaseEntryAction CreateAddGroupToCourseDatabaseEntryAction()
+        {
+            return new AddGroupToCourseDatabaseEntryAction(this.courseManagementRepostiories.Object);
+        }
+    }
+}
---
+++ trunk/code/CourseManagement/CourseManagement.EmailProcessing/Actions/AddGroupToCourseDatabaseEntryAction.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CourseManagement.Model;
+using CourseManagement.Persistence.Repositories;
+
+namespace CourseManagement.EmailProcessing.Actions
+{
+    class AddGroupToCourseDatabaseEntryAction
+    {
+        private ICourseManagementRepositories courseManagementRepositories;
+
+        public AddGroupToCourseDatabaseEntryAction(ICourseManagementRepositories courseManagementRepositories)
+        {
+            this.courseManagementRepositories = courseManagementRepositories;
+        }
+
+        public void Execute(IEmail email)
+        {
+
+            // Add group
+            int year = ParseYearFromEmail(email);
+            int semester = ParseSemesterFromEmail(email);
+
+            //TODO bajar los attachments
+
+            Group group = new Group(year, semester);
+
+            //verify existing group
+            var groupsInRepository = this.courseManagementRepositories.Groups.Get(g => (g.Semester == group.Semester) &&
+                                                                                       (g.Year == group.Year) &&
+                                                                                      (g.Id == group.Id) ).ToList();
+            if ( groupsInRepository == null)
+            {
+                Console.WriteLine("Es null");
+                Console.ReadLine();
+            }
+            if ( groupsInRepository.Count != 0)
+            {
+                throw new Exception("Trying to add to database an already existing group ");
+            }
+
+            this.courseManagementRepositories.Groups.Insert(group);
+
+            this.courseManagementRepositories.Groups.Save();
+
+        }
+
+        private int ParseSemesterFromEmail(IEmail email)
+        {
+            return 2;
+        }
+
+        private int ParseYearFromEmail(IEmail email)
+        {
+            return 2012;
+        }
+
+        private int ParseSubjectCodeFromEmail(IEmail email)
+        {
+            return 7510;
+        }
+    }
+}
