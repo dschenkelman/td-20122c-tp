41865ffc6324d0eddb87d285589e91f6a2cd3d1f
servetto.matias@gmail.com <servetto.matias@gmail.com@8d87b73c-e004-1360-0544-0c0580c6776a>
2012-10-25 04:56:09 +0000

    NewStudentToCourseDatabaseEntryActionFixture completed

--- code/CourseManagement/CourseManagement.EmailProcessing.Tests/Actions/NewStudentToCourseDatabaseEntryActionFixture.cs
+++ code/CourseManagement/CourseManagement.EmailProcessing.Tests/Actions/NewStudentToCourseDatabaseEntryActionFixture.cs
@@ -25,13 +25,19 @@ namespace CourseManagement.EmailProcessing.Tests.Actions
             this.studentRepository = this.mockRepository.Create<IRepository<Student>>();
             this.courseRepository = this.mockRepository.Create<IRepository<Course>>();
             this.courseManagementRepositories = this.mockRepository.Create<ICourseManagementRepositories>();
+
+            this.courseManagementRepositories.Setup(cmr => cmr.Students).Returns(this.studentRepository.Object);
+            this.courseManagementRepositories.Setup(cmr => cmr.Courses).Returns(this.courseRepository.Object);
         }
 
         [TestMethod]
         public void ShouldAddExistingStudentToDictatedCourse()
         {
             // arrange
-            Course course = new Course(2, 2012, 7510);
+            const int CourseCode = 7510;
+            const int CourseYear = 2012;
+            const int CourseSemester = 2;
+            Course course = new Course(CourseSemester, CourseYear, CourseCode);
             const string Name = "Sebastian Rodriguez";
             const string EmailAddress = "sebas312@hotmail.com";
             const int StudentId = 90202;
@@ -47,14 +53,18 @@ namespace CourseManagement.EmailProcessing.Tests.Actions
             NewStudentToCourseDatabaseEntryAction action = this.CreateNewStudentToCourseDatabaseEntryAction();
 
             Mock<IEmail> email = mockRepository.Create<IEmail>();
-            email.Setup(e => e.EmailSubject).Returns("[ALTA-MATERIA-CODIGO] " + StudentId + "-" + Name);
+            email.Setup(e => e.EmailSubject).Returns("[ALTA-MATERIA-" + CourseCode + "] " + StudentId + "-" + Name);
             email.Setup(e => e.Address).Returns(EmailAddress);
+            email.Setup(e => e.Date).Returns(new DateTime(CourseYear, 9, 5));
 
             // act
             action.Execute(email.Object);
 
             // assert
             Assert.AreEqual(1, course.Students.Count);
+            Assert.AreEqual(StudentId, course.Students.ElementAt(0).Id);
+            Assert.AreEqual(Name, course.Students.ElementAt(0).Name);
+            Assert.AreEqual(EmailAddress, course.Students.ElementAt(0).EmailAdress);
             
             // using linq method ElementAt because ICollection does not have a simpler way to index
             Assert.AreSame(student, course.Students.ElementAt(0));
@@ -69,39 +79,40 @@ namespace CourseManagement.EmailProcessing.Tests.Actions
         public void ShouldAddNoExistingStudentToDictatedCourse()
         {
             // arrange
-            Course course = new Course(2, 2012, 7510);
+            const int CourseCode = 7509;
+            const int CourseYear = 2011;
+            const int StudentId = 91363;
+            const string Name = "Matias";
+            Course course = new Course(1, CourseYear, CourseCode);
             List<Course> courses = new List<Course> { course };
 
             this.courseRepository.Setup(cr => cr.Get(It.IsAny<Expression<Func<Course, bool>>>())).Returns(courses).Verifiable();
             this.courseRepository.Setup(cr => cr.Save()).Verifiable();
-            this.studentRepository.Setup(sr => sr.GetById(91363)).Returns((Student)null).Verifiable();
-            // TODO me pide que mockee este metodo si pongo: It.Is<Student>(s => (s.Id == 91363) && (s.Name == "Matias Serveto"))
-            this.studentRepository.Setup(sr => sr.Insert(It.IsAny<Student>())).Verifiable();
+            this.studentRepository.Setup(sr => sr.GetById(StudentId)).Returns((Student)null).Verifiable();
+            this.studentRepository.Setup(sr => sr.Insert(It.Is<Student>(s => s.Id == StudentId && s.Name == Name))).Verifiable();
             this.studentRepository.Setup(sr => sr.Save()).Verifiable();
 
-            this.courseManagementRepositories.Setup(cmr => cmr.Students).Returns(this.studentRepository.Object);
-            this.courseManagementRepositories.Setup(cmr => cmr.Courses).Returns(this.courseRepository.Object);
             NewStudentToCourseDatabaseEntryAction action = this.CreateNewStudentToCourseDatabaseEntryAction();
 
             Mock<IEmail> email = mockRepository.Create<IEmail>();
-            const string Name = "Matias";
             const string EmailAddress = "servetto.matias@gmail.com";
-            email.Setup(e => e.EmailSubject).Returns("[ALTA-MATERIA-CODIGO] 91363-" + Name);
+            email.Setup(e => e.EmailSubject).Returns("[ALTA-MATERIA-" + CourseCode + "] " + StudentId + "-" + Name);
             email.Setup(e => e.Address).Returns(EmailAddress);
+            email.Setup(e => e.Date).Returns(new DateTime(CourseYear, 3, 14));
 
             // act
             action.Execute(email.Object);
 
             // assert
             Assert.AreEqual(1, course.Students.Count);
-            Assert.AreEqual(91363, course.Students.ElementAt(0).Id);
+            Assert.AreEqual(StudentId, course.Students.ElementAt(0).Id);
             Assert.AreEqual(Name, course.Students.ElementAt(0).Name);
             Assert.AreEqual(EmailAddress, course.Students.ElementAt(0).EmailAdress);
 
             this.courseRepository.Verify(cr => cr.Get(It.IsAny<Expression<Func<Course, bool>>>()), Times.Once());
             this.courseRepository.Verify(cr => cr.Save(), Times.Once());
-            this.studentRepository.Verify(sr => sr.GetById(91363), Times.Once());
-            this.studentRepository.Verify(sr => sr.Insert(It.IsAny<Student>()), Times.Once());
+            this.studentRepository.Verify(sr => sr.GetById(StudentId), Times.Once());
+            this.studentRepository.Verify(sr => sr.Insert(It.Is<Student>(s => s.Id == StudentId && s.Name == Name)), Times.Once());
             this.studentRepository.Verify(sr => sr.Save(), Times.Once());
         }
 
@@ -110,8 +121,9 @@ namespace CourseManagement.EmailProcessing.Tests.Actions
         public void ShouldRetrieveExceptionFromAddingAnAlreadyAddedStudentToCourse()
         {
             // arrange
-
-            Course course = new Course(2, 2012, 7510);
+            const int CourseCode = 7508;
+            const int CourseYear = 2010;
+            Course course = new Course(2, CourseYear, CourseCode);
             const string Name = "Damian Schenkelman";
             const string EmailAddress = "schen.damian@yahoo.com";
             const int StudentId = 90728;
@@ -119,22 +131,40 @@ namespace CourseManagement.EmailProcessing.Tests.Actions
             course.Students.Add( student );
             List<Course> courses = new List<Course> { course };
 
-            this.courseRepository.Setup(cr => cr.Get(It.IsAny<Expression<Func<Course, bool>>>())).Returns(courses);
-            this.courseRepository.Setup(cr => cr.Save()).Verifiable();
-            this.studentRepository.Setup(sr => sr.GetById(It.IsAny<int>())).Returns(student);
-            this.studentRepository.Setup(sr => sr.Insert(It.IsAny<Student>()));
-            this.studentRepository.Setup(sr => sr.Save()).Verifiable();
+            this.courseRepository.Setup(cr => cr.Get(It.IsAny<Expression<Func<Course, bool>>>())).Returns(courses).Verifiable();
+            this.studentRepository.Setup(sr => sr.GetById(It.IsAny<int>())).Returns(student).Verifiable();
 
-            this.courseManagementRepositories.Setup(cmr => cmr.Students).Returns(this.studentRepository.Object);
-            this.courseManagementRepositories.Setup(cmr => cmr.Courses).Returns(this.courseRepository.Object);
             NewStudentToCourseDatabaseEntryAction action = this.CreateNewStudentToCourseDatabaseEntryAction();
 
             Mock<IEmail> email = mockRepository.Create<IEmail>();
-            email.Setup(e => e.EmailSubject).Returns("[ALTA-MATERIA-CODIGO] " + StudentId + "-" + Name);
+            email.Setup(e => e.EmailSubject).Returns("[ALTA-MATERIA-" + CourseCode + "] " + StudentId + "-" + Name);
             email.Setup(e => e.Address).Returns(EmailAddress);
+            email.Setup(e => e.Date).Returns(new DateTime(CourseYear, 11, 19));
 
             // act
+            action.Execute(email.Object);
+        }
 
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void ShouldRetrieveExceptionFromTryingToSubscribeToAnInexistentCourse()
+        {
+            // arrange
+            List<Course> courses = new List<Course>();
+            this.courseRepository.Setup(cr => cr.Get(It.IsAny<Expression<Func<Course, bool>>>())).Returns(courses).Verifiable();
+
+            NewStudentToCourseDatabaseEntryAction action = this.CreateNewStudentToCourseDatabaseEntryAction();
+
+            Mock<IEmail> email = mockRepository.Create<IEmail>();
+            const string Name = "Damian Schenkelman";
+            const int StudentId = 90728;
+            const int CourseCode = 7508;
+            email.Setup(e => e.EmailSubject).Returns("[ALTA-MATERIA-" + CourseCode + "] " + StudentId + "-" + Name);
+            const string EmailAddress = "schen.damian@yahoo.com";
+            email.Setup(e => e.Address).Returns(EmailAddress);
+            email.Setup(e => e.Date).Returns(new DateTime(2010, 11, 19));
+
+            // act
             action.Execute(email.Object);
         }
 
