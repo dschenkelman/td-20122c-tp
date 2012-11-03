bfc0ecdf1a7c0084a09a280ce6a3bd9a58158a2b
sebastianr213@gmail.com <sebastianr213@gmail.com@8d87b73c-e004-1360-0544-0c0580c6776a>
2012-11-03 20:48:23 +0000

    NewGroupInCourseRule + Fixture Added rule mapping in App.config

---
+++ code/CourseManagement/CourseManagement.MessageProcessing/Rules/NewGroupInCourseRule.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using CourseManagement.MessageProcessing.Actions;
+using CourseManagement.Messages;
+
+namespace CourseManagement.MessageProcessing.Rules
+{
+    class NewGroupInCourseRule : BaseRule
+    {
+        private IActionFactory actionFactory;
+
+        private const string MatchingSubject = "[ALTA-GRUPO]";
+
+        public NewGroupInCourseRule(IActionFactory actionFactory) : base(actionFactory)
+        {
+            this.actionFactory = actionFactory;
+        }
+
+        public override bool IsMatch(IMessage message)
+        {
+            var subjectMatch = message.Subject.Equals(MatchingSubject);
+
+            var onlyOneAttachmentInMessage = (message.Attachments.Count() == 1);
+
+            var attachmentMatchExtensionTxt = false;
+
+            if ( onlyOneAttachmentInMessage )
+            {
+                var attachmentName = message.Attachments.First().Name;
+
+                var extension = "";
+                
+                try
+                {
+                    extension = Path.GetExtension(attachmentName);
+                    if ( extension != null)
+                    {
+                        attachmentMatchExtensionTxt = extension.Equals(".txt");    
+                    }
+                }catch (Exception)
+                {
+
+                }
+                
+            }
+            return (subjectMatch && onlyOneAttachmentInMessage && attachmentMatchExtensionTxt);
+        }
+    }
+}
