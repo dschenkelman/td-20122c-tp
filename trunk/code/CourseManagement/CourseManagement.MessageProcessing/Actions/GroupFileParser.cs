7f22e9f417a0831ddc73c6a7bbb8cfffa7295e8c
sebastianr213@gmail.com <sebastianr213@gmail.com@8d87b73c-e004-1360-0544-0c0580c6776a>
2012-11-03 19:35:02 +0000

    AddGroupToCourseDatabaseEntryAction

---
+++ trunk/code/CourseManagement/CourseManagement.MessageProcessing/Actions/GroupFileParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CourseManagement.Messages;
+
+namespace CourseManagement.MessageProcessing.Actions
+{
+    class GroupFileParser : IGroupFileParser
+    {
+        public IEnumerable<int> ObtainIdsFromMessage (IMessage message)
+        {
+            return ParseIdsFromAttachmentInMessage(message);
+        }
+        private static IEnumerable<int> ParseIdsFromAttachmentInMessage(IMessage message)
+        {
+            if (message.Attachments.Count() != 1)
+            {
+                throw new Exception("Message for Rule AddGroup doesn't have the unique attachment requirement");
+            }
+
+            var lines = message.Attachments.First().RetrieveLines();
+
+            return lines.Select(line => Convert.ToInt32(line)).ToList();
+        }
+    }
+}
